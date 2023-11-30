de8e1e4af28f16db525822c698e824a7d32b9373
Yaqub <yaqub399@my.yorku.ca>
2023-11-30 17:13:48 -0500

    UI for Catalog

--- catalog_service/src/main/resources/static/getrequest.js
+++ catalog_service/src/main/resources/static/getrequest.js
@@ -1,37 +1,34 @@
 GET: $(document).ready(
-    function() {
+    function ajaxGet() {
+        $.ajax({
+            type: "GET",
+            url: "entirecatalog",
+            success: function (result) {
+                if (result.status == "success") {
+                    $('#getResultDiv ul').empty();
+                    $.each(result.data, function (i, catalog) {
+                        var itemInfo = "Item ID: " + catalog.itemid +
+                            ", Seller ID: " + catalog.sellerid +
+                            ", Item Name: " + catalog.itemname +
+                            ", Item Description: " + catalog.itemdescription +
+                            ", Auction Type: " + catalog.auctiontype +
+                            ", Start Price: " + catalog.startprice +
+                            ", Shipping Price: " + catalog.shippingprice +
+                            ", Expedited Cost: " + catalog.expeditedcost +
+                            ", Duration: " + catalog.duration +
+                            ", End Date: " + catalog.enddate + "<br>";
 
-        // GET REQUEST
-        $("#getAllItems").click(function(event) {
-            event.preventDefault();
-            ajaxGet();
-        });
-
-        // DO GET
-        function ajaxGet() {
-            $.ajax({
-                type : "GET",
-                url : "entirecatalog",
-                success : function(result) {
-                    if (result.status == "success") {
-                        $('#getResultDiv ul').empty();
-                        var custList = "";
-                        $.each(result.data,
-                            function(i, catalag) {
-                                var user = "item Name  "+ catalog.itemname + ",Item Description : " + catalog.itemdescription + "<br>";
-                                $('#getResultDiv .list-group').append(
-                                    user)
-                            });
-                        console.log("Success: ", result);
-                    } else {
-                        $("#getResultDiv").html("<strong>Error</strong>");
-                        console.log("Fail: ", result);
-                    }
-                },
-                error : function(e) {
+                        $('#getResultDiv .list-group').append(itemInfo);
+                    });
+                    console.log("Success: ", result);
+                } else {
                     $("#getResultDiv").html("<strong>Error</strong>");
-                    console.log("ERROR: ", e);
+                    console.log("Fail: ", result);
                 }
-            });
-        }
+            },
+            error: function (e) {
+                $("#getResultDiv").html("<strong>Error</strong>");
+                console.log("ERROR: ", e);
+            }
+        });
     })
--- catalog_service/src/main/resources/static/postrequest.js
+++ catalog_service/src/main/resources/static/postrequest.js
@@ -1,44 +1,51 @@
-$(document).ready(
-    function() {
+$(document).ready(function () {
 
-        // SUBMIT FORM
-        $("#searchform").submit(function(event) {
-            // Prevent the form from submitting via the browser.
-            event.preventDefault();
-            ajaxPost();
-        });
+    // SUBMIT FORM
+    $("#searchform").submit(function (event) {
+        // Prevent the form from submitting via the browser.
+        event.preventDefault();
+        ajaxPost();
+    });
 
-        function ajaxPost() {
+    function ajaxPost() {
 
-            // PREPARE FORM DATA
-            var formData = {
-                itemname : $("#itemname").val(),
-            }
-
-            // DO POST
-            $.ajax({
-                type : "POST",
-                contentType : "application/json",
-                url : "saveBook",
-                data : JSON.stringify(formData),
-                dataType : 'json',
-                success : function(result) {
-                    if (result.status == "success") {
-                        $("#postResultDiv").html(
-                            "" + result.data.itemname
-                            + "Post Successfully! <br>"
-                            + "---> Congrats !!" + "</p>");
-                    } else {
-                        $("#postResultDiv").html("<strong>Error</strong>");
-                    }
-                    console.log(result);
-                },
-                error : function(e) {
-                    alert("Error!")
-                    console.log("ERROR: ", e);
-                }
-            });
-
+        // PREPARE FORM DATA
+        var formData = {
+            sellerid:$("#sellerid").val(),
+            itemname: $("#itemname").val(),
+            itemdescription: $("#itemdescription").val(),
+            auctiontype: $("#auctiontype").val(),
+            startprice: $("#startprice").val(),
+            shippingprice: $("#shippingprice").val(),
+            expeditedcost: $("#expeditedcost").val(),
+            duration: $("#duration").val(), // Assuming the input field for duration is of type text
+            enddate: $("#enddate").val() // Assuming the input field for enddate is of type text
         }
 
-    })
+        // DO POST
+        $.ajax({
+            type: "POST",
+            contentType: "application/json",
+            url: "sell",
+            data: JSON.stringify(formData),
+            dataType: 'json',
+            success: function (result) {
+                if (result.status == "success") {
+                    $("#postResultDiv").html(
+                        "" + result.data.itemname
+                        + "Post Successfully! <br>"
+                        + "---> Congrats !!" + "</p>");
+                } else {
+                    $("#postResultDiv").html("<strong>Error</strong>");
+                }
+                console.log(result);
+            },
+            error: function (e) {
+                alert("Error!")
+                console.log("ERROR: ", e);
+            }
+        });
+
+    }
+
+})
